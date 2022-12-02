314e9e6ca03823af4bc1e38a1c4178645af54cde
Rohit Gujarathi <gujju.rohit@gmail.com>
2022-12-02 15:03:53 -0800

    Added new icons

--- src/icons.h
+++ src/icons.h
@@ -2,8 +2,14 @@
 #ifndef _PAPERDINK_ICONS_H_
 #define _PAPERDINK_ICONS_H_
 
-/* Small icons are 24x24 px, med are 32x32 px */
+/* Icon sizes
+ * sml  are 24x24 px
+ * med  are 32x32 px 
+ * lrg  are 40x40 px
+ * xlrg are 48x48 px
+ */
 
+/* Icons are from https://feathericons.com/ */
 #include "icons/up_arrow_sml.h"
 #include "icons/down_arrow_sml.h"
 #include "icons/up_arrow_med.h"
@@ -13,5 +19,16 @@
 #include "icons/user_sml.h"
 #include "icons/user_med.h"
 #include "icons/wifi_off_sml.h"
+#include "icons/alert_xlrg.h"
+#include "icons/thermometer_med.h"
+#include "icons/thermometer_sml.h"
+#include "icons/umbrella_sml.h"
+#include "icons/snowflake_sml.h"
+#include "icons/sun_xlrg.h"
+#include "icons/drizzle_xlrg.h"
+#include "icons/lightning_xlrg.h"
+#include "icons/rain_xlrg.h"
+#include "icons/snow_xlrg.h"
+#include "icons/cloud_xlrg.h"
 
 #endif /* _PAPERDINK_ICONS_H_ */
